7f53a403d43336a031c8344430da1fa0c15142f9
Jacopo Clocchiatti <jacopo.clocchiatti@gmail.com>
2023-01-02 14:47:26 +0100

    refactor code to add argparsing

--- DatasetGeneration/generate_user_list.py
+++ DatasetGeneration/generate_user_list.py
@@ -1,24 +1,38 @@
 import uuid
 import os
+import argparse
 
 
-NUM_USER = 1000
 ASSET_DIR = "assets"
+USER_DIR = "users"
 
 
-def generate_user_list(num: int) -> str:
+def get_new_user(num: int) -> str:
     for _ in range(num):
         tmp_id = uuid.uuid4().time_low
         yield "user"+str(tmp_id)
 
 
-if __name__ == "__main__":
-    user_list = list(set(generate_user_list(NUM_USER)))
-    while len(user_list) != NUM_USER:
-        to_do = NUM_USER - len(user_list)
-        tmp = list(set(generate_user_list(to_do)))
+def generate_user_list(file_name: str, users_num: int):
+    user_list = list(set(get_new_user(users_num)))
+    while len(user_list) != users_num:
+        to_do = users_num - len(user_list)
+        tmp = list(set(get_new_user(to_do)))
         user_list.extend(tmp)
         user_list = list(set(user_list))
 
-    with open(os.path.join(ASSET_DIR, "user_list"), 'w') as fp:
+    with open(os.path.join(USER_DIR, file_name), 'w') as fp:
         fp.writelines("\n".join(user_list))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Script for users list generation")
+    parser.add_argument("--name", type=str, default='user_list',
+                        help="User list file name")
+    parser.add_argument("--num", type=str, default='5000',
+                        help="Number of users")
+    args = parser.parse_args()
+
+    user_file = args.name
+    number_of_users = int(args.num)
+    generate_user_list(user_file, number_of_users)
--- DatasetGeneration/generate_utility_matrix.py
+++ DatasetGeneration/generate_utility_matrix.py
@@ -1,14 +1,20 @@
 import csv
 import os
+from typing import List
+
 import numpy as np
 import argparse
 
 
 ASSETS_DIR = "assets"
+UTILITY_MATRICES_DIR = "utility_matrices"
+TABLE_DIR = "tables"
+QUERY_DIR = "queries"
+USER_DIR = "users"
 THRESHOLD = 0.33
 
 
-def get_utility_matrix(users, queries):
+def get_utility_matrix(users: List, queries: int, output_file_path: str):
     for user in users:
         row = [user]
         for _ in range(queries):
@@ -16,14 +22,14 @@ def get_utility_matrix(users, queries):
                 row.append(str(np.random.randint(0, 101)))
             else:
                 row.append("")
-        with open(os.path.join(ASSETS_DIR, "utility_matrix.csv"), 'a') as fp:
+        with open(os.path.join(UTILITY_MATRICES_DIR, output_file_path), 'a') as fp:
             fp.write(",".join(row))
             fp.write("\n")
 
 
-def read_queries(file_path):
+def read_queries(file_path: str, output_file_path: str) -> int:
     queries = []
-    with open(os.path.join(ASSETS_DIR, file_path), 'r') as fp:
+    with open(os.path.join(QUERY_DIR, file_path), 'r') as fp:
         csv_reader = csv.reader(fp)
         for row in csv_reader:
             if row:
@@ -32,7 +38,7 @@ def read_queries(file_path):
     queries = [query[0].split(",") for query in queries]
     queries_ids = [query[0] for query in queries]
     query_ids_len = len(queries_ids)
-    with open(os.path.join(ASSETS_DIR, "utility_matrix.csv"), 'w') as fp:
+    with open(os.path.join(UTILITY_MATRICES_DIR, output_file_path), 'w') as fp:
         fp.write(",".join(queries_ids))
         fp.write("\n")
 
@@ -41,9 +47,9 @@ def read_queries(file_path):
     return query_ids_len
 
 
-def read_users(file_path):
+def read_users(file_path: str) -> List:
 
-    with open(os.path.join(ASSETS_DIR, file_path), 'r') as fp:
+    with open(os.path.join(USER_DIR, file_path), 'r') as fp:
         user_list = fp.readlines()
     user_list = [user.strip() for user in user_list]
     return user_list
@@ -59,7 +65,8 @@ if __name__ == "__main__":
     queries_file = args.queries
     user_file = args.users
 
-    queries_num = read_queries(queries_file)
+    output_path = queries_file[:-4]+"_utility_matrix"+queries_file[-4:]
+    queries_num = read_queries(queries_file, output_path)
     users_list = read_users(user_file)
 
-    get_utility_matrix(users_list, queries_num)
+    get_utility_matrix(users_list, queries_num, output_path)

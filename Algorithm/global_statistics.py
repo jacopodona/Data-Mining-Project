bfb6e2e43f3a6ac889fe57097d214efeaee84315
Jacopo <49444372+jacopodona@users.noreply.github.com>
2023-01-18 17:23:51 +0100

    added evaluation losses and splits

--- Algorithm/global_statistics.py
+++ Algorithm/global_statistics.py
@@ -2,6 +2,7 @@ import pandas
 import os
 import csv
 import random
+import numpy as np
 import utils
 import pandas as pd
 import time
@@ -9,7 +10,7 @@ import time
 
 def displayDatasetStatistics(df):
     header=df.columns.values.tolist()
-    table=df.to_numpy()
+    table=df
     for col in header:
         print(col,len(pd.unique(df[col])))
 
@@ -17,6 +18,14 @@ def displayDatasetStatistics(df):
 if __name__ == '__main__':
     table = '../DatasetGeneration/tables/people.csv'
     queries = utils.read_queries('../DatasetGeneration/queries/people.csv')
+    matrix='../DatasetGeneration/utility_matrices/people_utility_matrix.csv'
 
     df=pd.read_csv(table)
-    displayDatasetStatistics(df)
+    utility_matrix=pd.read_csv(matrix)
+    #displayDatasetStatistics(df)
+    print('Generating train-val splits')
+    train_df,val_full_df=utils.get_train_val_split(utility_matrix,0.8)
+    print('Masking splits')
+    val_masked_df=utils.mask_val_split(val_full_df,0.5)
+    test_table = pd.DataFrame(np.ones(shape=np.shape(val_full_df.to_numpy())),columns=val_masked_df.columns.values.tolist())
+    print('Average error is: ',utils.evaluate(val_full_df,test_table))
--- Algorithm/utils.py
+++ Algorithm/utils.py
@@ -1,7 +1,10 @@
+import math
 import os
 import csv
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
 
 def read_queries(file_path):
     queries = []
@@ -16,7 +19,62 @@ def read_queries(file_path):
 
     return queries
 
-def get_evaluation_split(df):
+def get_train_val_split(table,train_user_percentage=0.7):
+    '''
+
+    :param table: pandas dataframe containing utility matrix
+    :param train_user_percentage: Percentage of users in training set with full available utility matrix
+    :return: train_dataframe, val_dataframe
+    '''
+    train_split=table[:int(len(table)*train_user_percentage)]
+    test_split=table[int(len(table)*train_user_percentage):]
+    return train_split,test_split
+
+def mask_val_split(df,query_split_percentage=0.5):
+    '''
+    portion of utility matrix of a fixed percentage of queries
+    :param df:
+    :param query_split_percentage:
+    :return:
+    '''
     header = df.columns.values.tolist()
-    table = df.to_numpy()
+
+    start_column_number=len(df.columns)
+    transposed=df.T
+    visible_split=transposed[:int(len(transposed)*query_split_percentage)]
+    masked_split=transposed[int(len(transposed)*query_split_percentage):]
+    visible_split=visible_split.T
+    masked_split=masked_split.T
+    numpy_table=visible_split.to_numpy()
+    final_table=np.empty(shape=np.shape(df.to_numpy()))
+    for i in range(0,len(numpy_table)):
+        row=numpy_table[i]
+        while(len(row)!=start_column_number):
+            row=np.append(row,float('nan'))
+        final_table[i]=row
+    masked_df= pd.DataFrame(final_table, columns = header)
+
+    return masked_df
+
+def evaluate(gt_df,proposal_df,huber_threshold=5):
+    error=0
+    gt_df=gt_df.to_numpy()
+    proposal_df = proposal_df.to_numpy()
+    for i in range(0,len(gt_df)):
+        row_error=0
+        gt_row=gt_df[i]
+        pr_row=proposal_df[i]
+        for j in range(len(gt_row)):
+            if(not math.isnan(gt_row[j])):
+                row_error+=abs((gt_row[j]-pr_row[j])) #Element wise MAE
+                #row_error+=(gt_row[j]-pr_row[j])**2 #Element wise MSE
+                '''delta=abs(gt_row[j]-pr_row[j])   #Huber method
+                if(delta<huber_threshold):
+                    row_error+=(gt_row[j]-pr_row[j])**2
+                else:
+                    row_error+=delta'''
+        error=row_error/len(gt_df)
+    return error
+
+
 
